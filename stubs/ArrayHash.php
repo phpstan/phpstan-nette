94fcf6860e5d60d15512064ee113033dbfc98223
Ondrej Mirtes <ondrej@mirtes.cz>
2020-01-11 15:56:30 +0100

    More stubs

---
+++ stubs/ArrayHash.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Nette\Utils;
+
+/**
+ * @implements \IteratorAggregate<mixed, mixed>
+ * @implements \ArrayAccess<mixed, mixed>
+ */
+class ArrayHash implements \ArrayAccess, \IteratorAggregate
+{
+
+	/**
+	 * @return \RecursiveArrayIterator<mixed>
+	 */
+	public function getIterator(): \RecursiveArrayIterator
+	{
+		// nothing
+	}
+
+}
