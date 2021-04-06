4930ff18725dd27bf2e15bb2b6ac1ef581d53514
Michal Špaček <mail@michalspacek.cz>
2021-04-06 03:12:59 +0200

    Dynamic return type extension for getUnsafeValues added in nette/forms 3.1.2
    getUnsafeValues unlike getValues requires a parameter and doesn't support booleans so I went with a new class instead of multiple ifs.

---
+++ src/Type/Nette/FormContainerUnsafeValuesDynamicReturnTypeExtension.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Type\Nette;
+
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Reflection\ParametersAcceptorSelector;
+use PHPStan\Type\ArrayType;
+use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\MixedType;
+use PHPStan\Type\NullType;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\StringType;
+use PHPStan\Type\Type;
+
+class FormContainerUnsafeValuesDynamicReturnTypeExtension implements DynamicMethodReturnTypeExtension
+{
+
+	public function getClass(): string
+	{
+		return 'Nette\Forms\Container';
+	}
+
+	public function isMethodSupported(MethodReflection $methodReflection): bool
+	{
+		return $methodReflection->getName() === 'getUnsafeValues';
+	}
+
+	public function getTypeFromMethodCall(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): Type
+	{
+		if (count($methodCall->args) === 0) {
+			return ParametersAcceptorSelector::selectSingle($methodReflection->getVariants())->getReturnType();
+		}
+
+		$arg = $methodCall->args[0]->value;
+		$scopedType = $scope->getType($arg);
+		if ($scopedType instanceof NullType) {
+			return new ObjectType('Nette\Utils\ArrayHash');
+		}
+
+		if ($scopedType instanceof ConstantStringType && $scopedType->getValue() === 'array') {
+			return new ArrayType(new StringType(), new MixedType());
+		}
+
+		return ParametersAcceptorSelector::selectSingle($methodReflection->getVariants())->getReturnType();
+	}
+
+}
