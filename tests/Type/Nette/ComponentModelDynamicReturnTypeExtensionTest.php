9f7c166dfc1f5beb7437dc4a9e367c2c22e7b535
Martin Jonas <martin.jonas@growjob.com>
2023-02-25 17:48:53 +0100

    Fixed component model return type extensions for cases where createComponent method is not present

---
+++ tests/Type/Nette/ComponentModelDynamicReturnTypeExtensionTest.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Type\Nette;
+
+use PHPStan\Testing\TypeInferenceTestCase;
+
+class ComponentModelDynamicReturnTypeExtensionTest extends TypeInferenceTestCase
+{
+
+	/**
+	 * @return iterable<string, mixed[]>
+	 */
+	public function dataFileAsserts(): iterable
+	{
+		yield from $this->gatherAssertTypes(__DIR__ . '/data/componentModel.php');
+	}
+
+	/**
+	 * @dataProvider dataFileAsserts
+	 * @param mixed ...$args
+	 */
+	public function testFileAsserts(
+		string $assertType,
+		string $file,
+		...$args
+	): void
+	{
+		$this->assertFileAsserts($assertType, $file, ...$args);
+	}
+
+	public static function getAdditionalConfigFiles(): array
+	{
+		return [
+			__DIR__ . '/phpstan.neon',
+		];
+	}
+
+}
