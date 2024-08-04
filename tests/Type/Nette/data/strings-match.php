bbe01f6e8b47285d32f5de73735326a15584d212
Markus Staab <maggus.staab@googlemail.com>
2024-08-04 09:44:31 +0200

    Understand constant strings in patterns

--- tests/Type/Nette/data/strings-match.php
+++ tests/Type/Nette/data/strings-match.php
@@ -15,21 +15,21 @@ function (string $s): void {
 	assertType('array{string, string}|null', $result);
 
 	$result = Strings::match($s, '/(foo)(bar)(baz)/', PREG_OFFSET_CAPTURE);
-	assertType('array{array{string, int<0, max>}, array{non-empty-string, int<0, max>}, array{non-empty-string, int<0, max>}, array{non-empty-string, int<0, max>}}|null', $result);
+	assertType("array{array{string, int<0, max>}, array{'foo', int<0, max>}, array{'bar', int<0, max>}, array{'baz', int<0, max>}}|null", $result);
 
 	$result = Strings::match($s, '/(foo)(bar)(baz)/');
-	assertType('array{string, non-empty-string, non-empty-string, non-empty-string}|null', $result);
+	assertType("array{string, 'foo', 'bar', 'baz'}|null", $result);
 
 	$result = Strings::match($s, '/(foo)(bar)'. preg_quote($s) .'(baz)/');
-	assertType('array{string, non-empty-string, non-empty-string, non-empty-string}|null', $result);
+	assertType("array{string, 'foo', 'bar', 'baz'}|null", $result);
 };
 
 function (string $s): void {
 	$result = Strings::matchAll($s, '/ab(?P<num>\d+)(?P<suffix>ab)?/', PREG_SET_ORDER);
-	assertType("list<array{0: string, num: numeric-string, 1: numeric-string, suffix?: non-empty-string, 2?: non-empty-string}>", $result);
+	assertType("list<array{0: string, num: numeric-string, 1: numeric-string, suffix?: 'ab', 2?: 'ab'}>", $result);
 };
 
 function (string $s): void {
 	$result = Strings::matchAll($s, '/ab(?P<num>\d+)(?P<suffix>ab)?/', PREG_PATTERN_ORDER);
-	assertType("array{0: list<string>, num: list<numeric-string>, 1: list<numeric-string>, suffix: list<string>, 2: list<string>}", $result);
+	assertType("array{0: list<string>, num: list<numeric-string>, 1: list<numeric-string>, suffix: list<''|'ab'>, 2: list<''|'ab'>}", $result);
 };
