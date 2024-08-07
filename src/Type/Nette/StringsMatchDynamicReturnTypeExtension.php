dbe0583f67f9b2a97a66a54a496759a4ef0c7bee
Kamil Žáček <kamil.zacek@gmail.com>
2024-08-07 14:43:14 +0200

    String::match / String::matchAll support for nette 4

--- src/Type/Nette/StringsMatchDynamicReturnTypeExtension.php
+++ src/Type/Nette/StringsMatchDynamicReturnTypeExtension.php
@@ -3,15 +3,21 @@
 namespace PHPStan\Type\Nette;
 
 use Nette\Utils\Strings;
+use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\StaticCall;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\MethodReflection;
 use PHPStan\TrinaryLogic;
+use PHPStan\Type\Constant\ConstantBooleanType;
+use PHPStan\Type\Constant\ConstantIntegerType;
 use PHPStan\Type\DynamicStaticMethodReturnTypeExtension;
 use PHPStan\Type\NullType;
 use PHPStan\Type\Php\RegexArrayShapeMatcher;
 use PHPStan\Type\Type;
 use PHPStan\Type\TypeCombinator;
+use function array_key_exists;
+use const PREG_OFFSET_CAPTURE;
+use const PREG_UNMATCHED_AS_NULL;
 
 class StringsMatchDynamicReturnTypeExtension implements DynamicStaticMethodReturnTypeExtension
 {
@@ -38,22 +44,48 @@ class StringsMatchDynamicReturnTypeExtension implements DynamicStaticMethodRetur
 	{
 		$args = $methodCall->getArgs();
 		$patternArg = $args[1] ?? null;
+
 		if ($patternArg === null) {
 			return null;
 		}
 
-		$flagsArg = $args[2] ?? null;
-		$flagsType = null;
-		if ($flagsArg !== null) {
-			$flagsType = $scope->getType($flagsArg->value);
-		}
+		$arrayShape = $this->regexArrayShapeMatcher->matchExpr(
+			$patternArg->value,
+			$this->resolveFlagsType($args, $scope),
+			TrinaryLogic::createYes(),
+			$scope
+		);
 
-		$arrayShape = $this->regexArrayShapeMatcher->matchExpr($patternArg->value, $flagsType, TrinaryLogic::createYes(), $scope);
 		if ($arrayShape === null) {
 			return null;
 		}
 
 		return TypeCombinator::union($arrayShape, new NullType());
+	}
+
+	/**
+	 * @param array<Arg> $args
+	 */
+	private function resolveFlagsType(array $args, Scope $scope): ConstantIntegerType
+	{
+		if (!array_key_exists(2, $args)) {
+			return new ConstantIntegerType(0);
+		}
+
+		$captureOffsetType = $scope->getType($args[2]->value);
+
+		if ($captureOffsetType instanceof ConstantIntegerType) {
+			return $captureOffsetType;
+		}
+
+		$unmatchedAsNullType = array_key_exists(4, $args) ? $scope->getType($args[4]->value) : new ConstantBooleanType(false);
+
+		$captureOffset = $captureOffsetType->isTrue()->yes();
+		$unmatchedAsNull = $unmatchedAsNullType->isTrue()->yes();
+
+		return new ConstantIntegerType(
+			($captureOffset ? PREG_OFFSET_CAPTURE : 0) | ($unmatchedAsNull ? PREG_UNMATCHED_AS_NULL : 0)
+		);
 	}
 
 }
