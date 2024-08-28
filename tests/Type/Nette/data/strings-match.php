f975b0604d6da630f26ecbb9c1efecd782fec339
Markus Staab <maggus.staab@googlemail.com>
2024-08-28 10:15:51 +0200

    Fix test expectations

--- tests/Type/Nette/data/strings-match.php
+++ tests/Type/Nette/data/strings-match.php
@@ -40,6 +40,6 @@ function (string $s): void {
 };
 
 function (string $s): void {
-	$result = Strings::matchAll($lineContent, '~\[gallery ids=(„|")(?<allIds>([0-9]+,? ?)+)(“|")~');
-	assertType('list<array{0: string, 1: non-empty-string, allIds: non-empty-string, 2: non-empty-string, 3: non-empty-string, 4: non-empty-string}>', $result);
+	$result = Strings::matchAll($s, '~\[gallery ids=(„|")(?<allIds>([0-9]+,? ?)+)(“|")~');
+	assertType("list<array{0: string, 1: '"'|'„', allIds: non-empty-string, 2: non-empty-string, 3: non-empty-string, 4: '"'|'“'}>", $result);
 };
